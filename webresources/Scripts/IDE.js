64fac31b4919cdf6119c0fcf7fc758a6724cd9b5
Eragon_peerGynt <eragon.peergynt@gmail.com>
2020-01-08 23:09:20 +0100

    workin turing machine in most basic form, will need to improve UI and tms itself

---
+++ webresources/Scripts/IDE.js
@@ -0,0 +1,61 @@
+
+var lines = 0;
+$(document).ready(function(){
+
+    var textArea = $("#codeText"),
+        textAreaY = textArea.scrollTop();
+
+    addRows($("#codeText").val().split("\n").length);
+    lines = $("#codeText").val().split("\n").length;
+    $("#codeText").on("input", function(e) {
+        var input = $(this);
+        var nrRows = input.val().split("\n").length;
+
+        if (lines != nrRows) {
+            if (lines < nrRows) {
+                addRows(nrRows);
+            }
+            else {
+                removeRows(nrRows);
+            }
+            lines = nrRows;
+        }
+
+    });
+    $("#codeText").scroll(function() {
+        $(".numbering").css("margin-top", (3-Number(textArea.scrollTop())).toString()+"px");
+        visibilityCleaner();
+    });
+});
+var addRows = function(newRows) {
+    for (i = lines+1; i <= newRows; i++) {
+        $(".numbering").append("<div id='number" + i + "'><div class='row'>"+ i +"</div></div>");
+    }
+}
+
+var visibilityCleaner = function() {
+    $(".row").each(function() {
+        var topArea = $("#codeText").offset().top, bottomArea = $("#codeText").offset().top+$("#codeText").height();
+        console.log(bottomArea);
+        if($(this).offset().top < topArea || $(this).offset().top > bottomArea) {
+            $(this).css({'visibility':'hidden', 'opacity': '0'});
+            $(this).parent().css({'visibility':'hidden', 'opacity': '0'});
+        }
+        else {
+            $(this).css({'visibility':'visible', 'opacity': '1'});
+            $(this).parent().css({'visibility':'visible', 'opacity': '1'});
+        }
+    });
+}
+var removeRows = function(newRows) {
+    $(".row").each(function() {
+        if ($(this).html() > newRows) {
+            $(this).parent().remove();
+        }
+    });
+}
+
+var updateRows = function() {
+    lines = $("#codeText").val().split("\n").length;
+    removeRows(lines);
+}
---
+++ webresources/Scripts/TMS.js
@@ -0,0 +1,123 @@
+$(document).ready(function(){
+    reset_machine();
+    $('.config__box').mousedown(handle_mousedown);
+    $('.new__button').click(function(){
+        var answer = window.confirm('Do you really want to clear IDE window?');
+        if (answer) {
+            $('#codeText').val("");
+            updateRows();
+        }
+    });
+    $('.set__button').click(function(){
+        if ($(".tms__configuration").is(":hidden")) {
+            return;
+        }
+        $(".tms__configuration").hide()
+        set_memory_registry();
+    });
+    $('.reset__button').click(function(){
+        $(".tms__configuration").show()
+    });
+    $('.step__button').click(function(){
+        if ($(".tms__configuration").is(":visible")) {
+            return;
+        }
+        step_machine();
+    });
+});
+
+var reset_machine = function() {
+    $(".head_value").each(function(){
+        $(this).remove();
+    });
+    $(".tape__registry").each(function(){
+        $(this).remove();
+    });
+    $(".config__input__soft").each(function(){
+        $(this).remove();
+    });
+    for (let i = 1; i <= number_of_heads; i++) {
+        $('.scanner__head').append("<div class='head__" + i + " head_value'></div>");
+        $('.registry').append("<div class='tape__registry'><pre class='tape__" + i + " tape__left'></pre>"+"<pre class='tape__" + i + " tape__select'></pre>"+"<pre class='tape__" + i + " tape__right'></pre></div>")
+        $('.tms__configuration').append("<input type='text' class='config__input config__input__soft initial__input" + i + "' value='' />");
+    }
+}
+
+var set_memory_registry = function() {
+    for (let i = 1; i <= number_of_heads; i++) {
+        let tape_preset = $('.initial__input' + i).val().split("");
+        if (tape_preset.length > 0) {
+            $('.tape__' + i + '.tape__select').html(tape_preset[0]);
+        }
+        else {
+            $('.tape__' + i + '.tape__select').html("_");
+        }
+        if (tape_preset.length > 1) {
+            $('.tape__' + i + '.tape__right').html(tape_preset.slice(1).join(""));
+        }
+    }
+}
+
+var step_machine = function() {
+    let program = $("#codeText").val().split("\n")
+    let head_vission = [];
+    for (let i = 1; i <= number_of_heads; i++) {
+        head_vission.push($('.tape__' + i + '.tape__select').html());
+    }
+    let execute_command = custom_regex(current_state, head_vission, program);
+    let command_setters = execute_command[0].split(",");
+    let command_movers = execute_command[1].split(',');
+    let command_new_state = execute_command[2];
+    head_writter(command_setters);
+    head_mover(command_movers);
+    current_state = command_new_state;
+}
+
+var custom_regex = function(state, current_head, program) {
+    for (let command of program) {
+        if (custom_comparator(state, current_head, command.split(" ").slice(0,2))) {
+            return command.split(" ").slice(2);
+        }
+    }
+    return null;
+}
+
+var custom_comparator = function(state, current_head, command) {
+    if (state == command[0]) {
+        let command_blob = command[1].split(",");
+        for (let j = 0; j < current_head.length; j++) {
+            if (command_blob[j] != current_head[j]) {
+                return false;
+            }
+        }
+        return true;
+    }
+    return false;
+}
+
+var head_writter = function(new_values) {
+    for (let i = 1; i <= number_of_heads; i++) {
+        if (new_values[i] != "*") {
+            $('.tape__' + i + '.tape__select').html(new_values[i])
+        }
+    }
+}
+
+var head_mover = function(moves) {
+    for (let i = 1; i <= number_of_heads; i++) {
+        let left = $('.tape__' + i + '.tape__left').html();
+        let right = $('.tape__' + i + '.tape__right').html();
+        let select = $('.tape__' + i + '.tape__select').html();
+        if (moves[i-1] == "r") {
+            $('.tape__' + i + '.tape__left').html(left + select);
+            $('.tape__' + i + '.tape__select').html(right.split("")[0]);
+            $('.tape__' + i + '.tape__right').html(right.split("").slice(1).join(""));
+        }
+        else if (moves[i-1] == "l") {
+            let left_length = left.split("").length
+            $('.tape__' + i + '.tape__right').html(select + right);
+            $('.tape__' + i + '.tape__select').html(left.split("")[left_length-1]);
+            $('.tape__' + i + '.tape__left').html(left.split("").slice(0, left_length-1).join(""));
+        }
+    }
+}
